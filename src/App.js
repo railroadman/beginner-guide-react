2c98409cea1a1396767fe0e82a2d4e9ba32ae216
Volodymyr Rozdobudko <volodymyrr@nayax.coom>
2024-11-17 01:58:49 +0200

    currency-conbvertor almost done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { Block } from './Block'
 import './index.scss'
 
 function App() {
-  const [fromCurrency, setFromCurrency] = useState('HRN')
+  const [fromCurrency, setFromCurrency] = useState('UAH')
   const [toCurrency, setToCurrency] = useState('USD')
-  const [fromPrice, setFromPrice] = useState(0)
-  const [toPrice, setToPrice] = useState(0)
-  const [rates, setRates] = useState({})
+  const [fromPrice, setFromPrice] = useState(1)
+  const [toPrice, setToPrice] = useState(1)
+  //const [rates, setRates] = useState({})
+
+  const ratesRef = React.useRef({})
   const API_KEY = '28f72f08c78e48289a7bd08e1df0fe0e'
   //https://api.currencyfreaks.com/v2.0/rates/latest?apikey=
   useEffect(() => {
     fetch('http://localhost:8880/json')
       .then(res => res.json())
       .then(json => {
-        setRates(json.rates)
-        console.log(json.rates)
+        ratesRef.current = json.rates
+        //setRates(json.rates)
       })
       .catch(err => {
         console.warn(err)
@@ -23,17 +25,26 @@ function App() {
   }, [])
 
   const onChangeFromPrice = value => {
-    const price = value / rates[fromCurrency]
-    const result = price * rates[toCurrency]
-    setToPrice(result)
+    const price = value / ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
+    setToPrice(result.toFixed(2))
     setFromPrice(value)
   }
 
   const onChangeToPrice = value => {
-    const result = (rates[fromCurrency] / rates[toCurrency]) * value
-    setFromPrice(result)
+    const result =
+      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+    setFromPrice(result.toFixed(2))
     setToPrice(value)
   }
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+
+  useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
 
   return (
     <div className='App'>
